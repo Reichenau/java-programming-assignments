c7c2540a480583a8f25478bb2cd86fde5d30fd01
Reichenau <schildt515@gmail.com>
2025-11-22 01:57:20 +0300

    Введение/Консольный ввод – вывод/Практика 1

---
+++ src/intro/console_input_output/practice01/Main.java
@@ -0,0 +1,14 @@
+package intro.console_input_output.practice01;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Введите ваше имя: ");
+
+        String name = scan.next();
+
+        System.out.println("Привет, " + name);
+    }
+}
