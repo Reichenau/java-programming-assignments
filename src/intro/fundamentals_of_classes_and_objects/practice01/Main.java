7d5d61025fedfd86e468dfa9c7137e4fc86d4a26
Reichenau <schildt515@gmail.com>
2025-11-22 01:46:19 +0300

    Введение/Основы классов и объектов/Практика 1

---
+++ src/intro/fundamentals_of_classes_and_objects/practice01/Main.java
@@ -0,0 +1,35 @@
+package intro.fundamentals_of_classes_and_objects.practice01;
+
+class MessageSender {
+
+    public void sendMessage(String text) {
+        System.out.println("Отправка сообщения: " + text);
+    }
+}
+
+class EmailSender extends MessageSender {
+
+    @Override
+    public void sendMessage(String text) {
+        System.out.println("Отправка Email: " + text);
+    }
+}
+
+class SmsSender extends MessageSender {
+
+    @Override
+    public void sendMessage(String text) {
+        System.out.println("Отправка СМС: " + text);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        MessageSender sender = new EmailSender();
+
+        sender.sendMessage("Текстовое сообщение ");
+
+        sender = new SmsSender();
+        sender.sendMessage("1234");
+    }
+}
