757cfb6cd3162ef7c7f6c1fbde88b236bac5d242
Daniel <schildt515@gmail.com>
2025-12-05 01:05:11 +0300

    Строки/Форматирование информации/Практика 1,3

--- src/strings/formatting/practice01/Main.java
+++ src/strings/formatting/practice01/Main.java
@@ -1,4 +1,28 @@
+/*
+Строки -> Форматирование информации -> Практика 1
+Практика #1
+Приведите пример использования пяти любых спецификаторов из таблицы.
+*/
 package strings.formatting.practice01;
 
 public class Main {
+    public static void main(String[] args) {
+        String name = "Java";
+        int version = 21;
+        double price = 0.0;
+        boolean isGood = true;
+
+        // 1. %s - Строка
+        // 2. %d - Целое число
+        // 3. %f - Число с точкой
+        // 4. %b - Логическое значение
+        // 5. %n - Перенос строки
+
+        System.out.printf("Язык: %s%n", name);
+        System.out.printf("Версия: %d%n", version);
+        System.out.printf("Цена: %.2f руб.%n", price);
+        System.out.printf("Объектно ориентированный? %b%n", isGood);
+
+        System.out.printf("Итог: %s версии %d - это %b!%n", name, version, isGood);
+    }
 }
--- src/strings/formatting/practice03/Main.java
+++ src/strings/formatting/practice03/Main.java
@@ -1,4 +1,26 @@
+/*
+Строки -> Форматирование информации -> Практика 3
+Практика #3
+Приведите пример использования пяти любых спецификаторов из таблицы.
+*/
 package strings.formatting.practice03;
 
+import java.util.Date;
+
 public class Main {
+    public static void main(String[] args) {
+        Date now = new Date();
+
+
+        // 1. %tY - Год
+        // 2. %tB - Полное название месяца
+        // 3. %tA - Полное название дня недели
+        // 4. %tH - Час
+        // 5. %tM - Минуты
+
+        System.out.printf("Сегодня: %tA, %tB %tY года%n", now, now, now);
+        System.out.printf("Точное время: %tH:%tM%n", now, now);
+
+        System.out.printf("Коротко: %1$td.%1$tm.%1$ty", now);
+    }
 }
