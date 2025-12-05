7c37321bf730b5d083e417de6d9dd90a25cb93c3
Daniel <schildt515@gmail.com>
2025-12-05 14:03:37 +0300

    Исключения и ошибки/Собственные исключения/Практика 1

---
+++ src/exceptions_and_errors/custom_exceptions/practice01/Main.java
@@ -0,0 +1,44 @@
+/*
+Исключения и ошибки -> Собственные исключения -> Практика 1
+Практика #1
+Приведите пример собственного класса исключения.
+*/
+package exceptions_and_errors.custom_exceptions.practice01;
+
+class MyFirstException extends Exception {
+
+    // 1. Пустой конструктор
+    public MyFirstException() {
+        super();
+    }
+
+    // 2. Конструктор с сообщением
+    public MyFirstException(String message) {
+        super(message);
+    }
+
+    // 3. Конструктор с сообщением и причиной (другим исключением)
+    public MyFirstException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Проверка возраста: ");
+            checkAge(-5);
+        } catch (MyFirstException e) {
+            System.out.println("Перехват собственного исключения");
+            System.out.println("Сообщение: " + e.getMessage());
+        }
+    }
+
+    public static void checkAge(int age) throws MyFirstException {
+        if (age < 0) {
+            // Используем второй конструктор (с сообщением)
+            throw new MyFirstException("Возраст не может быть отрицательным");
+        }
+        System.out.println("Возраст принят: " + age);
+    }
+}
